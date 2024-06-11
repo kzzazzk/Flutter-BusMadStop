c1ff8b03e62cbfe97c71d42c8aafb1ee3f1c7546
kzzazzk <zsahraoui20@gmail.com>
2024-06-11 23:29:38 +0200

    Changed UI of BusStopScreen

--- lib/screens/bus_stop_screen.dart
+++ lib/screens/bus_stop_screen.dart
@@ -9,47 +9,173 @@ class BusStopScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(feature['properties']['DENOMINACION']),
+        backgroundColor: Colors.red,
+        automaticallyImplyLeading: false, // Remove the default back button
+        title: Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+            const Align(
+              alignment: Alignment.center,
+              child: Icon(
+                Icons.directions_bus,
+                color: Colors.white,
+                size: 45,
+              ),
+            ),
+          ],
+        ),
       ),
       body: content(),
     );
   }
 
   Widget content() {
-    return Center(
+    List<String> busLines = feature['properties']['LINEAS'].split(', ');
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text(
-            'Bus Stop Information',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40),
+                  child: Text(
+                    feature['properties']['DENOMINACION'],
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    feature['properties']['CODIGOEMPRESA'],
+                    style: const TextStyle(fontSize: 15, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Wrap(
+                  spacing: 10,
+                  runSpacing: 10,
+                  children: busLines.map((line) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 3, horizontal: 6),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        line,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ],
             ),
           ),
-          SizedBox(height: 20),
-          Text(
-            'Bus Stop Name: ${feature['properties']['DENOMINACION']}',
-            style: TextStyle(
-              fontSize: 20,
-            ),
+          const SizedBox(
+            height: 20,
           ),
-          SizedBox(height: 10),
-          Text(
-            'Bus Stop Address: ${feature['properties']['NOMBREVIA']}',
-            style: TextStyle(
-              fontSize: 20,
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Bus Stop Routes: ${feature['properties']['LINEAS']}',
-            style: TextStyle(
-              fontSize: 20,
+            child: Column(
+              children: [
+                ...busLines.map((line) {
+                  // Placeholder for the latest time for each bus. Replace with actual data.
+                  String latestTime = '-';
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Text(
+                            line,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          latestTime,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ],
             ),
           ),
         ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: BusStopScreen(
+      feature: const {
+        'properties': {
+          'DENOMINACION': 'Main St',
+          'CODIGOEMPRESA': '123456',
+          'LINEAS': '1, 2, 3'
+        }
+      },
+    ),
+  ));
 }
